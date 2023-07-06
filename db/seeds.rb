883588077194f331a26b998985709a3aae42a3b0
Brenda <brendakajuju777@gmail.com>
2023-07-06 15:22:34 +0300

    add Hero Powers Controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,4 @@ Hero.create(name: 'Gwen Stacy', superhero_name: 'Spider-Gwen')
 
 Power.create(name: 'Super strength', description: 'a monster hunter for hire')
 Power.create(name: 'Blood Magic', description: 'a powerful sorceress who is half elf and half mage and can summon the old magic')
+Power.create(name: 'Bard', description: 'a gifted instrumentalist that inspires citizens to be hopeful in times of war')
