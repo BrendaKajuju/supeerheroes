b04fd2e5ddb9e11071ed8e58ddfb1844c493097c
Brenda <brendakajuju777@gmail.com>
2023-07-06 15:21:45 +0300

    add hero controller

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,15 @@
 class HerosController < ApplicationController
+  def index
+    heros = Hero.all
+    render json: heros, only: [:id, :name, :superhero_name]
+  end
+
+  def show
+    hero = Hero.includes(:powers).find_by(id: params[:id])
+    if hero
+      render json: hero.as_json(include: { powers: { only: [:id, :name, :description] } })
+    else
+      render json: { error: 'Hero not found' }, status: :not_found
+    end
+  end
 end
